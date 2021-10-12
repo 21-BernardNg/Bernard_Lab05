53f48929cf6dce428ef6ba53d5a9135d2a7ff446
21-BernardNg <bernardngboonhao@gmail.com>
2021-10-12 17:34:11 +0800

    Created CollisionScript
    Implemented coin collision, add 10 to score when trigger enter a coin.
    
    Created UI CoinText, updates with current coin score value.
    
    Implemented collision with water, player loses and move to GameLoseScene.
    
    Added function to set cursor visibility and unlock it when reaching GameWinScene or GameLoseScene.

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -5,6 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class ButtonScript : MonoBehaviour
 {
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
     public void RestartButton()
     {
         SceneManager.LoadScene("GameScene");
---
+++ Assets/Scripts/CollisionScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CollisionScript : MonoBehaviour
+{
+    public Text coinText;
+    public int coin;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Coin")
+        {
+            Destroy(other.gameObject);
+
+            coin += 10;
+            coinText.text = $"Coin : {coin}";
+        }
+
+        if (other.gameObject.tag == "Water")
+        {
+            SceneManager.LoadScene("GameLoseScene");
+        }
+    }
+}
